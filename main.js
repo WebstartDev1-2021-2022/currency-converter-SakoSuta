e926ec43ec2c3ce186aa77b266a1eb21d68bd542
Emilie <emiliemontpre@outlook.com>
2022-01-11 15:53:27 +0100

    modification

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const switchCurrencies = (event) =>
     event.preventDefault()
     //TODO: Inverser les monnaies from et to
     console.log('le bouton switch a été cliqué')
+    [inputFrom.value, inputTo.value] = [inputTo.value, inputFrom.value]
 }
 
 const submitForm = async (event) => 
@@ -30,7 +31,8 @@ const submitForm = async (event) =>
     //TODO: Vérifier que currencyFrom et currencyTo ne sont pas égaux
     if (currencyFrom === currencyTo)
     {
-
+        alert('Vous n\'avez pas changer la valeur !')
+        return
     }
 
     //TODO: Afficher une alerte si le champ from est vide
@@ -47,7 +49,9 @@ const submitForm = async (event) =>
         const response = await fetch(`https://api.frankfurter.app/latest?amount=${fromValue}&from=${currencyFrom}&to=${currencyTo}`)
         const json = await response.json()
         console.log('Réponse complète :', json)
-        console.log('Résultat :',json.rates[currencyTo])
+        console.log('Résultat :', json.rates[currencyTo])
+
+        inputTo.value = json.rates[currencyTo]
     }
 
     catch (error)
@@ -57,9 +61,66 @@ const submitForm = async (event) =>
     }
 }
 
+const updateSelectValue = async () => 
+{
+    // //Requête Ajax (Asynchronous Js and Xml)
+    // fetch('https://api.frankfurter.app/currencies')
+    //     .then((result) => result.json())
+    //     .then((data) => 
+    //     {
+    //         // Je crée une chaîne de craratères vide
+    //         let htmlContent = ''
+
+    //         // Je remplis la chaîne de caractère avec des balises HTLM utilisant les clés et les valeurs de chaque entrée de l'objet
+
+    //         for( const [key, value] of Object.entries(data))
+    //         {
+    //             const option = `<option value="${key}">${value}</option>`
+    //             htmlContent += option
+    //         }
+    //         console.log(htmlContent)
+
+    //         //Je remplace le HTML des deux menus select par htmlContent
+    //         selectFromCurrencies.innerHTML = htmlContent
+    //         selectToCurrencies.innerHTML = htmlContent
+    //     })
+    //     .catch((error) => console.error('Erreur updateSelectValue : ',error))
+
+    try 
+    {
+        // Stocker le résultat de la promesse json() dans la constante result
+        const result = await fetch('https://api.frankfurter.app/currencies')
+
+        // Stocker le résultat de la promesse json() dans la constante data
+        const data = await result.json()
+
+        let htmlContent = ''
+
+        // Je remplis la chaîne de caractère avec des balises HTLM utilisant les clés et les valeurs de chaque entrée de l'objet
+
+        for( const [key, value] of Object.entries(data))
+        {
+            const option = `<option value="${key}">${value}</option>`
+            htmlContent += option
+        }
+
+        //Je remplace le HTML des deux menus select par htmlContent
+        selectFromCurrencies.innerHTML = htmlContent
+        selectToCurrencies.innerHTML = htmlContent
+
+    } 
+    catch (error) 
+    {
+        console.error('Erreur updateSlectValue :', error)
+    }
+}
+
 //TODO: traiter la réponse de l'API en remplissant le champ from
 switchButton.addEventListener("click", switchCurrencies)
 form.addEventListener("submit", submitForm)
+
+//Appel de fonctions au chargement de la page
+updateSelectValue()
 
 // switchButton.addEventListener
 // (
